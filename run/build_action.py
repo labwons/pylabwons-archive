8f6503e0d0975402d5ab36641ead8bbb19ec0bd3
JeHyeuk <JeHyeuk.Lee@hyundai-kefico.com>
2026-03-10 09:03:35 +0900

    [labwons.manager]

--- run/build_action.py
+++ run/build_action.py
@@ -4,8 +4,8 @@ import os
 
 
 if __name__ == "__main__":
-    if not lws.HOST == 'hkefico':
-        lw.login_krx(os.environ['KRX_ID'], os.environ['KRX_PW'])
+
+    lw.login_krx(os.environ['KRX_ID'], os.environ['KRX_PW'])
 
     baseline = lws.Baseline(logger=lw.Logger(console=False))
     baseline.number.progress_bar = False
